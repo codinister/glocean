58e6d48e7e97cadae8417dc0c1700e5082703707
codinister <codinister@gmail.com>
2025-06-04 08:46:53 +0100

    Commit 5

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -3,18 +3,32 @@
 import useGetQuery from '@/data/query/useGetQuery';
 import Image from 'next/image';
 import Link from 'next/link';
-import {usePathname} from 'next/navigation'
+import { usePathname } from 'next/navigation';
+import { useEffect, useState } from 'react';
 
 const Nav = () => {
   const data = useGetQuery('/settings', 'settings');
-  const path = usePathname()
+  const path = usePathname();
+  const [visibility, setVisibility] = useState('hide');
 
-  
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
 
+  const handleScroll = () => {
+    const scrolly = window.scrollY;
+    const scroll = Math.floor(scrolly);
+    if (scroll > 150) {
+      document.querySelector('nav')?.classList.add('navbg');
+    } else {
+      document.querySelector('nav')?.classList.remove('navbg');
+    }
+  };
 
   return (
     <nav>
-      <div className="container navbar">
+      <div className={`container navbar ${visibility}`}>
         <div>
           {data[0]?.image ? (
             <Image src={data[0]?.image} alt="logo" width="100" height="70" />
@@ -25,28 +39,48 @@ const Nav = () => {
         <div>
           <ul>
             <li>
-              <Link className={path === '/' ? 'active' : ''} href="/">Home</Link>
+              <Link className={path === '/' ? 'active' : ''} href="/">
+                Home
+              </Link>
             </li>
             <li>
-              <Link className={path === '/about' ? 'active' : ''} href="/about">About</Link>
+              <Link className={path === '/about' ? 'active' : ''} href="/about">
+                About
+              </Link>
             </li>
             <li>
-              <Link className={path === '/contact' ? 'active' : ''} href="/contact">Contact</Link>
+              <Link
+                className={path === '/contact' ? 'active' : ''}
+                href="/contact"
+              >
+                Contact
+              </Link>
             </li>
             <li>
-              <Link className={path === '/track' ? 'active' : ''} href="/track">Track Shipment</Link>
+              <Link className={path === '/track' ? 'active' : ''} href="/track">
+                Track Shipment
+              </Link>
             </li>
           </ul>
         </div>
       </div>
 
-      <div className="backdrop"></div>
+      <div
+        onClick={() => setVisibility('hide')}
+        className={`backdrop ${visibility}`}
+      ></div>
 
       <div className="hamburger">
         <div>
-          <Image src="/hamburger.jpg" alt="Hamburger" width="5" height="5" />
+          <Image
+            src="/hamburger.jpg"
+            alt="Hamburger"
+            width="30"
+            height="30"
+            onClick={() => setVisibility('show')}
+          />
         </div>
-        <div>{data[9]?.comp_name}</div>
+        <div>{data[0]?.company_name}</div>
       </div>
     </nav>
   );
